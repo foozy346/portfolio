91fe5d29ac2a64fad61544d37c560b3244e35384
foozy346 <fawzy346@gmail.com>
2025-03-11 13:08:35 +0200

    add edit skills php for android users

--- skills.php
+++ skills.php
@@ -29,6 +29,11 @@
 <div class="container-fluid ">
     <div class="row justify-content-around align-items-center">
         <?php
+        
+        function isMobile() {
+            return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+        }
+        
         $skilllls = array(
             "C",
             "C++",
@@ -78,16 +83,25 @@
             "blender.png",
         );
         for($i=0; $i < count($skilllls) ; $i++){
-            echo '<div class="card text-center border border-info" style="width: 11rem; margin: 25px;"> </br>';
+            echo '<div class="card text-center border border-info" ';
+            if(isMobile()) echo 'style="width: 5rem; margin: 5px;"> </br>'; 
+            else echo 'style="width: 11rem; margin: 25px;"> </br>';
+            
             if ($skilllls[$i] == "Proteus" || $skilllls[$i] == "PHP" ){
-                echo'<center><img class="card-img-top center"  style="height: 60px; width: 80px; " src="/files/'.$skil_pic[$i].'" ></center>';
+                echo'<center><img class="card-img-top center"';
+                if(isMobile()) echo ' style="height: 30px; width: 40px; " src="/files/'.$skil_pic[$i].'" ></center>';
+                else echo ' style="height: 60px; width: 80px; " src="/files/'.$skil_pic[$i].'" ></center>';
             }else{
-                echo'<center><img class="card-img-top center"  style="height: 60px; width: 60px; " src="/files/'.$skil_pic[$i].'" ></center>';
+                echo'<center><img class="card-img-top center"  ';
+                if(isMobile()) echo' style="height: 30px; width: 30px; " src="/files/'.$skil_pic[$i].'" ></center>';
+                else echo' style="height: 60px; width: 60px; " src="/files/'.$skil_pic[$i].'" ></center>';
             }
-            echo'<div class="card-body">
-                    <h5 class="card-text" >'.$skilllls[$i].'</h5>
-                </div>
-                </div>';
+            echo'<div class="card-body">';
+            if(isMobile())  echo '<b class="card-text" style= "font-size:0.5em;">'.$skilllls[$i].'</b>';
+            else echo '<h5 class="card-text" >'.$skilllls[$i].'</h5>';
+            
+            echo ' </div>
+                   </div>';
         } 
         ?>
     </div>
